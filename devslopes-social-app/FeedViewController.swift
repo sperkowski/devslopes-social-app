bd361572f489754650b25a38d4b56a81b7b9336d
sperkowski <sperkowski@gmail.com>
2016-10-10 21:03:19 -0700

    completed Firebaser display to PostCell

--- devslopes-social-app/FeedViewController.swift
+++ devslopes-social-app/FeedViewController.swift
@@ -48,10 +48,15 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let post = posts[indexPath.row]
-        print("STEVE: \(post.caption)")
         
-        return tableView.dequeueReusableCell(withIdentifier: "PostCell")!
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
     }
+    
     
     
     
