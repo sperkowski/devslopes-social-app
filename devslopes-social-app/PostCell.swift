8962cecd7c1c95246ca6fbf121f10f666a70a4aa
sperkowski <sperkowski@gmail.com>
2016-10-11 06:56:46 -0700

    Completed Firebase storage download with image cache

--- devslopes-social-app/PostCell.swift
+++ devslopes-social-app/PostCell.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class PostCell: UITableViewCell {
     
@@ -23,11 +24,32 @@ class PostCell: UITableViewCell {
         // Initialization code
     }
 
-    func configureCell(post: Post) {
+    // default value for img is nil if nothing is passed in
+    func configureCell(post: Post, img: UIImage? = nil) {
         self.post = post
         self.caption.text = post.caption
         self.likesLbl.text = "\(post.likes)"
     
-    }
+        if img != nil {
+            self.postImg.image = img
+        } else {
+            let ref = FIRStorage.storage().reference(forURL: post.imageUrl)
+            ref.data(withMaxSize: 2 * 1024 * 1024, completion: { (data, error ) in
+                if error != nil {
+                    print("STEVE: unable to downloand image from Firebase Storage")
+                } else {
+                    print("STEVE: image downloaded from Firebase Storage")
+                    if let imgData = data {
+                        if let img = UIImage(data: imgData) {
+                            self.postImg.image = img
+                            FeedViewController.imageCache.setObject(img, forKey: post.imageUrl as NSString)
+                        }
+                    }
+                }
+            })
+        }
+        
+        }
+    
     
 }
